0af895c801593870e23931f2eed3a0ce4d343572
ayz1070 <ayz1070@gmail.com>
2025-05-13 22:04:24 +0900

    [refactor] 출석 데이터 소스 코드 분리
    - dio 전역 선언 및 의존성 주입
    - DioError 분리

--- lib/features/attendance/data/data_source/attendance_remote_data_source.dart
+++ lib/features/attendance/data/data_source/attendance_remote_data_source.dart
@@ -1,6 +1,5 @@
 import 'package:dio/dio.dart';
-import 'package:logger/logger.dart';
-
+import '../../../../core/error/error_handler.dart';
 import '../dto/attendance_dto.dart';
 import '../dto/create_attendance_request_dto.dart';
 import '../dto/delete_attendance_request_dto.dart';
@@ -8,101 +7,41 @@ import 'attendance_data_source.dart';
 
 class AttendanceRemoteDataSource implements AttendanceDataSource {
   final Dio dio;
-  final Logger logger = Logger();
 
-  AttendanceRemoteDataSource({required String baseUrl})
-      : dio = Dio(BaseOptions(
-    baseUrl: "$baseUrl/api/v1",
-    connectTimeout: const Duration(seconds: 10),
-    receiveTimeout: const Duration(seconds: 10),
-    responseType: ResponseType.json,
-    headers: {'Content-Type': 'application/json'},
-  )) {
-    dio.interceptors.add(
-      InterceptorsWrapper(
-        onRequest: (options, handler) {
-          logger.i('[Request] ${options.method} ${options.uri}');
-          logger.d('Headers: ${options.headers}');
-          logger.d('Body: ${options.data}');
-          return handler.next(options);
-        },
-        onResponse: (response, handler) {
-          logger.i('[Response] ${response.statusCode} ${response.requestOptions.uri}');
-          logger.d('Response Data: ${response.data}');
-          return handler.next(response);
-        },
-        onError: (DioException e, handler) {
-          logger.e('[Error] ${e.message}');
-          if (e.response != null) {
-            logger.e('Error Response: ${e.response?.data}');
-          }
-          return handler.next(e);
-        },
-      ),
-    );
-  }
+  AttendanceRemoteDataSource({required this.dio});
 
-  static const String _attendanceEndpoint = '/attendances';
+  static const String _endpoint = '/attendances';
 
   @override
   Future<List<AttendanceDto>> fetchAttendances() async {
     try {
-      final response = await dio.get('$_attendanceEndpoint');
-      final List<dynamic> data = response.data as List<dynamic>;
-      return data.map((item) => AttendanceDto.fromJson(item)).toList();
+      final response = await dio.get(_endpoint);
+      final List<dynamic> data = response.data;
+      return data.map((e) => AttendanceDto.fromJson(e)).toList();
     } on DioException catch (e) {
-      logger.e(_handleDioError(e, '출석 리스트 조회 실패'));
-      throw Exception(_handleDioError(e, '출석 리스트 조회 실패'));
+      throw Exception(handleDioError(e, '출석 리스트 조회 실패'));
     }
   }
 
   @override
   Future<AttendanceDto> createAttendance(CreateAttendanceRequestDto request) async {
     try {
-      final response = await dio.post(
-        '$_attendanceEndpoint',
-        data: request.toJson(),
-      );
+      final response = await dio.post(_endpoint, data: request.toJson());
       return AttendanceDto.fromJson(response.data);
     } on DioException catch (e) {
-      logger.e(_handleDioError(e, '출석 생성 실패'));
-      throw Exception(_handleDioError(e, '출석 생성 실패'));
+      throw Exception(handleDioError(e, '출석 생성 실패'));
     }
   }
 
   @override
   Future<void> deleteAttendance(DeleteAttendanceRequestDto request) async {
     try {
-      final response = await dio.delete(
-        '$_attendanceEndpoint',
-        queryParameters: request.toQueryParameters(),
-      );
+      final response = await dio.delete(_endpoint, queryParameters: request.toQueryParameters());
       if (response.statusCode != 204) {
-        logger.w('출석 삭제 실패: ${response.statusCode}');
         throw Exception('출석 삭제 실패: ${response.statusCode}');
       }
     } on DioException catch (e) {
-      logger.e(_handleDioError(e, '출석 삭제 실패'));
-      throw Exception(_handleDioError(e, '출석 삭제 실패'));
+      throw Exception(handleDioError(e, '출석 삭제 실패'));
     }
-  }
-
-  // 에러 메시지 처리
-  String _handleDioError(DioException e, String defaultMessage) {
-    if (e.response != null) {
-      switch (e.response?.statusCode) {
-        case 400:
-          return '$defaultMessage: 잘못된 요청입니다.';
-        case 401:
-          return '$defaultMessage: 인증되지 않은 사용자입니다.';
-        case 403:
-          return '$defaultMessage: 권한이 없습니다.';
-        case 404:
-          return '$defaultMessage: 요청한 출석 데이터를 찾을 수 없습니다.';
-        case 500:
-          return '$defaultMessage: 서버 오류가 발생했습니다.';
-      }
-    }
-    return '$defaultMessage: ${e.message}';
   }
 }
