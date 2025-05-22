8e965a645536bac913b10cd51fcceaeeb3ca34cb
Jun <80628496+ayz1070@users.noreply.github.com>
2025-05-22 16:03:11 +0900

    [ui] 구글 로그인, 카카오 로그인 버튼 ui 구현

---
+++ lib/features/sign_up/presentation/widgets/social_login_button.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+import '../../../../core/theme/app_text_styles.dart';
+
+
+class SocialLoginButton extends StatelessWidget {
+  final dynamic icon;
+  final String label;
+  final Color backgroundColor;
+  final Color textColor;
+  final Color? borderColor;
+  final VoidCallback onPressed;
+
+  const SocialLoginButton({
+    super.key,
+    required this.icon,
+    required this.label,
+    required this.backgroundColor,
+    required this.textColor,
+    this.borderColor,
+    required this.onPressed,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onPressed,
+      child: Container(
+        width: double.infinity,
+        padding: EdgeInsets.symmetric(vertical: 12),
+        decoration: BoxDecoration(
+          color: backgroundColor,
+          borderRadius: BorderRadius.circular(8),
+          border: borderColor != null ? Border.all(color: borderColor!) : null,
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            SvgPicture.asset(
+              icon,
+              width: 24,
+              height: 24,
+            ),
+            SizedBox(width: 6),
+            Text(label,
+                style: AppTextStyles.body2.copyWith(color: textColor)),
+          ],
+        ),
+      ),
+    );
+  }
+}
