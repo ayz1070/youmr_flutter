9d88ef42cf8dffeeef97e7547aa45abccf85e8b9
ayz1070 <ayz1070@gmail.com>
2025-05-27 22:55:46 +0900

    [fix] 출석 시 ui에 바로 적용되지 않는 이슈 해결
    -

--- lib/features/attendance/presentation/viewmodel/attendance_view_model.dart
+++ lib/features/attendance/presentation/viewmodel/attendance_view_model.dart
@@ -1,5 +1,6 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:youmr_flutter/features/attendance/domain/usecase/delete_attendance_use_case.dart';
+import 'package:youmr_flutter/features/splash/presentation/provider/member_provider.dart';
 
 import '../../domain/usecase/create_attendance_use_case.dart';
 import '../../domain/usecase/fetch_attendance_use_case.dart';
@@ -9,56 +10,62 @@ class AttendanceViewModel extends StateNotifier<AttendanceState> {
   final CreateAttendanceUseCase createAttendanceUseCase;
   final DeleteAttendanceUseCase deleteAttendanceUseCase;
   final FetchAttendancesUseCase fetchAttendancesUseCase;
+  final Ref ref;
 
   AttendanceViewModel({
     required this.createAttendanceUseCase,
     required this.deleteAttendanceUseCase,
     required this.fetchAttendancesUseCase,
+    required this.ref,
   }) : super(AttendanceState());
 
   Future<void> fetchAttendances() async {
     final entities = await fetchAttendancesUseCase.call();
-    final attendeeList =
-        entities
-            .map(
-              (e) => {
-                'name': e.memberName,
-                'profileImageUrl': e.memberProfileImageUrl,
-              },
-            )
-            .toList();
+    final attendeeList = entities
+        .map((e) => {
+      'name': e.memberName,
+      'profileImageUrl': e.memberProfileImageUrl,
+    })
+        .toList();
 
     state = state.copyWith(attendees: attendeeList);
   }
 
   Future<Map<String, String>> attend() async {
-    const int memberId = 1;
+    final member = ref.read(memberProvider);
+    if (member == null) {
+      throw Exception('로그인된 사용자가 없습니다.');
+    }
 
     await createAttendanceUseCase.call(
-      memberId: memberId,
+      memberId: member.id!, // 🔥 null 아님 보장 필요
       attendanceDate: DateTime.now(),
       attendanceType: 'DEFAULT',
     );
 
-    final entities = await fetchAttendancesUseCase.call();
-    final newEntity = entities.last; // 방금 출석한 정보라 가정
-    return {
-      'name': newEntity.memberName,
-      'profileImageUrl': newEntity.memberProfileImageUrl,
+    final newAttendee = {
+      'name': member.name,
+      'profileImageUrl': member.profileImage,
     };
+
+    state = state.copyWith(attendees: [...state.attendees, newAttendee]);
+
+    return newAttendee;
   }
 
   Future<void> cancelAttend() async {
-    int memberId = 1;
+    final member = ref.read(memberProvider);
+    if (member == null) {
+      throw Exception('⚠ 로그인된 사용자가 없습니다.');
+    }
 
     await deleteAttendanceUseCase.call(
-      memberId: memberId,
+      memberId: member.id!,
       attendanceDate: DateTime.now(),
-      attendanceType: "DEFAULT",
+      attendanceType: 'DEFAULT',
     );
 
+    // 최신 출석 리스트 갱신
     await fetchAttendances();
   }
-
-
 }
